6000ebb3d3fba518aed6822b36c9e2163197e283
Aqueel Miqdad <amiqdad@hawk.iit.edu>
2017-03-13 21:13:11 -0500

    sdsds

---
+++ todo.js
@@ -0,0 +1,16 @@
+var todos = [
+    {todo: 'CS 429 Assignment', done: false, index: 1},
+    {todo: 'Tuesday Trip', done: false, index: 2},
+    {todo: 'Dominion Game Night', done: false, index: 3},
+];
+
+var curr_index = 3;
+
+const myTodos = document.getElementById('todos');
+
+function hello() {
+    for (todo in todos) {
+        document.createElement('div', )
+    }
+}
+
