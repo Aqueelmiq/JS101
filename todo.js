9dccf781d8176f47936254ac5aa1a5724fcbed4e
Aqueel Miqdad <amiqdad@hawk.iit.edu>
2017-03-13 21:44:36 -0500

    latest

--- todo.js
+++ todo.js
@@ -5,12 +5,47 @@ var todos = [
 ];
 
 var curr_index = 3;
-
 const myTodos = document.getElementById('todos');
 
 function hello() {
-    for (todo in todos) {
-        document.createElement('div', )
+
+    for (var i=0; i<todos.length; i++) {
+        addTodo(todos[i].todo);
     }
 }
 
+const input = document.getElementById('input');
+const addbtn = document.getElementById('add');
+
+addbtn.addEventListener('click', function () {
+    const pp = input.value;
+    console.log(pp);
+    todos.unshift({todo: pp, done: false, index: ++curr_index});
+    addTodo(pp)
+})
+
+function addTodo(todo) {
+    const li = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    checkbox.className = "check";
+    checkbox.addEventListener('')
+    const todotext = document.createElement('p');
+    todotext.textContent = todo;
+    todotext.className = "todo-text";
+    const del = document.createElement('a');
+    del.textContent = "delete";
+    li.appendChild(checkbox);
+    li.appendChild(todotext);
+    li.appendChild(del);
+    li.className = "todo";
+    //myTodos.appendChild(li);
+    myTodos.insertBefore(li, myTodos.firstChild)
+
+}
+
+function check() {
+ 
+}
+
+hello();
